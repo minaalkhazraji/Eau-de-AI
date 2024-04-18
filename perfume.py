43a71c3a465163ad587cf006486a7c93bef0e10a
minaalkhazraji <minaalkhazraji@ufl.edu>
2024-04-17 20:35:28 -0400

    combined reva's perfume class into rec engine, initialized functions for edge perfumes for graph

--- perfume.py
+++
@@ -1,6 +0,0 @@
-class Perfume:
-    def __init__(self, name, notes, price, occasions):
-        self.name = name
-        self.notes = set(notes) #create a set of a bunch of notes
-        self.price = price
-        self.occasions = set(occasions)
--- recommendation_engine.py
+++ recommendation_engine.py
@@ -1,3 +1,10 @@
+class Perfume:
+    def __init__(self, name, notes, price, occasions):
+        self.name = name
+        self.notes = set(notes) #create a set of a bunch of notes
+        self.price = price
+        self.occasions = set(occasions)
+
 class Hash:
 
     def __init__(self):
@@ -20,8 +27,43 @@ class Graph:
         if perfume not in self.adjacency_list:
             self.adjacency_list[perfume] = []
 
-    def addEdge(self, perfume1,perfume2):
+    def add_edge(self, perfume1,perfume2):
         if perfume1 != perfume2:
             self.adjacency_list[perfume1].append(perfume2)
             self.adjacency_list[perfume2].append(perfume1)
 
+    def find_edge(self, perfume):
+        #get similar perfumes from graph
+        return self.adjacency_list.get(perfume, [])
+
+class RecommendationEngine:
+    def __init__(self, perfumes):
+        self.perfumes = perfumes
+        self.hash = Hash()
+        self.graph = Graph()
+        self.build_methods()
+
+    def build_methods(self):
+        for perfume in self.perfumes:
+            self.hash.add_perfume(perfume)
+            self.graph.add_edge(perfume)
+            self.add_similar_edges(perfume)
+
+    def add_similar_edges(self, perfume):
+        #checks for similar perfumes amongst existing perfumes
+        existing_perfumes = list(self.graph.adjacency_list.keys())
+        for existing_perfume in existing_perfumes:
+            if any(note in existing_perfume.notes for note in perfume.notes):
+                self.graph.add_edge(perfume, existing_perfume)
+
+    def recommend(self, notes, price_range, ocassions, method):
+        if method == 'graph':
+            return self.recommend_graph(notes, price_range, ocassions)
+        elif method == 'hash':
+            return self.recommend_hash(notes, price_range, ocassions)
+
+    def recommend_graph(self, notes, price_range, ocassions):
+        #graph recommendation logic
+
+    def recommend_hash(self, notes, price_range, ocassions):
+        #hash recommendation logic
