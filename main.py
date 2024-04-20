294b6d2412eb472fba9bb8c18aeba7e421187820
reva <reva.adi@ufl.edu>
2024-04-20 15:36:07 -0400

    changed the occasion options to daytime,night,anytime...

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_user_preferences():
     price_range = ', '.join(selected_prices)
 
     #occasion selection
-    occasion_options = ["Casual", "Formal", "Evening", "Sport"]
+    occasion_options = ["Daytime", "Nightime","Anytime"]
     print("\nSelect the occasion:")
     for i, option in enumerate(occasion_options, 1):
         print(f"{i}. {option}")
